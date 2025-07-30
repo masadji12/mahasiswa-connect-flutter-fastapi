14b167e64b3a2bfa64f4854e544f2bc9a4aa1f6f
masadji12 <mmasadjifirmansyah@gmail.com>
2025-07-30 20:44:15 +0700

    Update controlles create endpoint post mahasiswa

--- webservice_fastapi/controllers/mahasiswa_controller.py
+++ webservice_fastapi/controllers/mahasiswa_controller.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from models.mahasiswa_model import Mahasiswa
+from models.mahasiswa_model import Mahasiswa, CreateMahasiswa
 from db.database import get_connection
 
 route = APIRouter()
@@ -15,5 +15,19 @@ def get_all_mahasiswa():
         cursor.close()
         con.close()
         return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@route.post("/mahasiswa")
+def create_mahasiswa(m: CreateMahasiswa):
+    try:
+        con = get_connection()
+        cursor = con.cursor()
+        query = "INSERT INTO mahasiswa (nama, nim, id_prov, angkatan, tinggi_badan) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(query, (m.nama, m.nim, m.id_prov, m.angkatan, m.tinggi_badan))
+        con.commit()
+        cursor.close()
+        con.close()
+        return {"Pesan": "Mahasiswa Berhasil ditambahkan"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
